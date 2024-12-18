047789239bad8562359a784143fc766f37484ac3
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-12-18 09:20:30 +0530

    division of number

---
+++ CPP Practicals/Assigenment 1/05_division.cpp
@@ -0,0 +1,20 @@
+//write program division of number
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b,div;
+    cout<<"Enter the First Number:-"<<endl;
+    cin>>a;
+    cout<<"Enter the Second Number:-"<<endl;
+    cin>>b;
+    div = a / b;
+    cout<<"Your division "<<a<<" / "<<b<<" = "<<div;
+
+    return 0;
+}
+
+// Enter the First Number:-
+// 10
+// Enter the Second Number:-
+// 2
+// Your division 10 / 2 = 5
