f7c013c0d594851c4502f2720070806eba56c234
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-12-18 09:23:17 +0530

    circumference of circle

---
+++ CPP Practicals/Assigenment 1/10_circumference_of_circle.cpp
@@ -0,0 +1,14 @@
+// write program to calculate the circumference of circle 
+#include<iostream>
+using namespace std;
+int main(){
+    int circum,r;
+    cout<<"Enter the redius of circle:-"<<endl;
+    cin>>r;
+    circum = 2 * 3.14 * r;
+    cout<<"circumference of ciurcle:-"<<circum;
+    return 0;
+}
+// Enter the redius of circle:-
+// 2
+// circumference of ciurcle:-12
