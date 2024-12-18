07d2c38ef3509f86c00c66b3545df2a629c3942f
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-12-18 09:19:17 +0530

    subtraction two number

---
+++ CPP Practicals/Assigenment 1/03_subtraction.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sub;
+    cout<<"Enter the First Number:-"<<endl;
+    cin>>a;
+    cout<<"Enter the Second Number:-"<<endl;
+    cin>>b;
+    sub = a - b;
+    cout<<"Your subtraction "<<a<<" - "<<b<<" = "<<sub;
+
+    return 0;
+}
+// Enter the First Number:-
+// 5
+// Enter the Second Number:-
+// 2
+// Your subtraction 5 - 2 = 3
