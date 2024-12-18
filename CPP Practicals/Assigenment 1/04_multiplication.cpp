d88f5f76c4d3ca4e66176724d8180b3d84a35c92
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-12-18 09:19:58 +0530

    mulitiplication of two number

---
+++ CPP Practicals/Assigenment 1/04_multiplication.cpp
@@ -0,0 +1,20 @@
+// write program to mulitplication of two number
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,mul;
+    cout<<"Enter the First Number:-"<<endl;
+    cin>>a;
+    cout<<"Enter the Second Number:-"<<endl;
+    cin>>b;
+    mul = a * b;
+    cout<<"Your multiplication "<<a<<" * "<<b<<" = "<<mul;
+
+    return 0;
+}
+
+// Enter the First Number:-
+// 10
+// Enter the Second Number:-
+// 2
+// Your multiplication 10 * 2 = 20
