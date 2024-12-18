811a88596331dc0398514d82fdf2a2fc256c85ae
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-12-18 09:21:08 +0530

    area of recatangle

---
+++ CPP Practicals/Assigenment 1/06_area_rectanangle.cpp
@@ -0,0 +1,18 @@
+// write program area of rectangle
+#include<iostream>
+using namespace std;
+int main(){
+    int l,w,area;
+    cout<<"Enter the length:-"<<endl;
+    cin>>l;
+    cout<<"Enter the width:-"<<endl;
+    cin>>w;
+    area = l * w;
+    cout<<"Area of rectangle "<<l<<" * "<<w<<" = "<<area;
+    return 0;
+}
+// Enter the length:-
+// 2
+// Enter the width:-
+// 4
+// Area of rectangle 2 * 4 = 8
