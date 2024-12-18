d14de3d44e46f9ce23889a6588abc9a045dd5c4e
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-12-18 09:18:46 +0530

    addition of two number

---
+++ CPP Practicals/Assigenment 1/02_addition.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{  int a,b,sum;
+   cout<<"Enter the First Number:-"<<endl;
+   cin>>a;
+   cout<<"Enter the Second NUmber:-"<<endl;
+   cin>>b;
+   sum = a + b;
+   cout<<"Your Sum "<<a<<"+"<<b<<"="<<sum;
+    return 0;
+}
