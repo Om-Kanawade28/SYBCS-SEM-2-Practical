1a54bde0802466367f312924352516cce5e26fec
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2024-12-18 09:22:09 +0530

    area of circle

---
+++ CPP Practicals/Assigenment 1/08_area_of_circle.cpp
@@ -0,0 +1,14 @@
+//write program to area of circle 
+#include<iostream>
+using namespace std;
+int main(){
+    int r,area;
+    cout<<"Enter the redious of circle :-"<<endl;
+    cin>>r;
+    area = 3.14 * r * r;
+    cout<<"Area of circle = "<<area;
+    return 0;
+}
+// Enter the redious of circle :-
+// 4
+// Area of circle = 50
