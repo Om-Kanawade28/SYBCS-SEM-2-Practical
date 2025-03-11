6183e066becad9176d519c8d39c41ef1ff3b1efd
Om-Kanawade28 <154841806+Om-Kanawade28@users.noreply.github.com>
2025-03-11 13:55:19 +0530

    Update 01_forward_difference_table.py

--- Advanced math practicles/assigenment 6/01_forward_difference_table.py
+++ Advanced math practicles/assigenment 6/01_forward_difference_table.py
@@ -7,12 +7,12 @@ for i in range(n):
     x[i]=float(input('x['+str(i)+']='))
     y[i][0]=float(input('y['+str(i)+']='))
 for i in range(1,n):
-    for j in range(1,n-1):
+    for j in range(1,n-i):
         y[j][i] = y[j+1][i-1]-y[j][i-1]
 print("\nFORWARD DIFFERNCE TABLE\n")
 for i in range(0,n):
     print('%0.2f'%(x[i]),end='')  # Print x values
-    for j in range(0,n-1):
+    for j in range(0,n-i):
         print('\t\t%0.2f'%(y[i][j]),end='')  # Print y values (differences)
     print()
 
